2d91af113306faca1cf35cb0fb11546eed5e57e0
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2025-06-28 10:45:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Simple catch-all route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'chat.html'));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
